272c54fba538a53db1c041b3af7d73e6b8ef4afa
gautamyash <gautamyash37@gmail.com>
2024-02-08 14:30:33 +0530

    Project

--- src/components/Add User/AddUser.js
+++ src/components/Add User/AddUser.js
@@ -4,7 +4,6 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css";
-
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
@@ -44,7 +43,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <>
       {error && (
         <ErrorModal
           title={error.title}
@@ -71,7 +70,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/Helpers/Wrapper.js
@@ -0,0 +1,5 @@
+const Wrapper = (props) => {
+  return props.chidren;
+};
+
+export default Wrapper;
